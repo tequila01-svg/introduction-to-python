5dae2fdb72d7ecd0fac677a24478f4207de93915
tequila01-svg <onifadeayobami008@gmail.com>
2024-10-04 18:03:22 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#this is my first python code
+#this is a comment
+print("hello, world!")
+#keywords
+print("keywords in python are........")
+import keyword
+print(keyword.kwlist)
+print(9)
+print("i am a sentence /n i am a second section")
